77dddcd70133f974b8533b314ecd47ec8a20f6fe
Shea <984794777@qq.com>
2020-05-25 13:39:47 +0800

    firtst commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {  Row, Col  } from 'antd';
+import 'antd/dist/antd.css';
+import './App.css'
+import{
+  Header,
+} from './components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Row style={{marginBottom:'4px'}}>
+        <Col span={6}>
+          <div className='content'></div>
+        </Col>
+        <Col span={12}>
+          <div className='content'></div>
+        </Col>
+        <Col span={6}>
+          <div className='content'></div>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={8}>
+          <div className='content'></div>
+        </Col>
+        <Col span={8}>
+          <div className='content'></div>
+        </Col>
+        <Col span={8}>
+          <div className='content'></div>
+        </Col>
+      </Row>
+    </>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header(){
+    return (
+        <>
+        <h1>
+            test
+        </h1>
+        </>
+    )
+}
